168ed560a54c6e7c5b96e665d01034e7df8aa3ed
rinat.balbaev <rinat.balbaev@mail.ru>
2020-06-16 14:55:59 +0400

    Done

---
+++ Lesson 1_1 Rinat.py
@@ -0,0 +1,10 @@
+my_str1 = 'Hi there!'
+my_int1 = 777
+my_bool1 = True
+
+print(my_str1, my_int1, my_bool1)
+
+user_str1 = input('String: ')
+user_int1 = int(input('Integer: '))
+
+print('Result: ', 'String - ', user_str1, ', Integer - ', user_int1)
