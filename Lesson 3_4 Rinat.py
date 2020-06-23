90d08b38fe3a71ff56a8e256efedf16e2d911832
rinat.balbaev <rinat.balbaev@mail.ru>
2020-06-23 16:50:32 +0400

    Done

--- Lesson 3_4 Rinat.py
+++
@@ -1,7 +0,0 @@
-def my_func(x, y):
-    res = 1
-    for i in range(abs(y)):
-        res *= x
-    return 1 / res
-
-print(my_func(float(input('Enter positive integer: ')), abs(int(input('Enter negative power(integer): '))) * (-1)))
